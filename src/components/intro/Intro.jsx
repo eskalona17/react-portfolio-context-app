1a6bcf8a9ba060ba4125ff0a9193fa324b635398
eskalona17 <eskalona_17@hotmail.com>
2021-10-25 14:26:42 +0200

    intro section in progress

---
+++ src/components/intro/Intro.jsx
@@ -0,0 +1,39 @@
+import "./intro.css"
+import Me from "../../img/me.png"
+
+const Intro = () => {
+    return (
+        <div className="i">
+            <div className="i-left">
+                <div className="i-left-wrapper">
+                    <h2 className="i-intro">Hello, My name is</h2>
+                    <h1 className="i-name">John Doe</h1>
+                    <div className="i-title">
+                        <div className="i-title-wrapper">
+                            <div className="i-title-item">Web Developer</div>
+                            <div className="i-title-item">UI/UX Designer</div>
+                            <div className="i-title-item">Photographer</div>
+                            <div className="i-title-item">Writer</div>
+                            <div className="i-title-item">Content Creator</div>
+                        </div>
+                    </div>
+                    <div className="i-desc">
+                        Lorem ipsum dolor sit amet consectetur 
+                        adipisicing elit. Sapiente fugiat, aperiam 
+                        odio praesentium hic laboriosam veritatis 
+                        eveniet porro inventore nihil repellat 
+                        incidunt architecto corporis repudiandae 
+                        tempore dolorum molestias totam reiciendis.
+                    </div>
+                </div>
+            </div>
+            <div className="i-right">
+                <div className="i-bg"></div>
+                <img src={Me} alt="" className="i-img" />
+            </div>
+        </div>
+    )
+}
+
+export default Intro
+
