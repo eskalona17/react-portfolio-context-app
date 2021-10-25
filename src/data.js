b06da853a95cb56a18e71c900391cd98c4ec9f29
eskalona17 <eskalona_17@hotmail.com>
2021-10-25 23:21:53 +0200

    doing portfolio section

---
+++ src/data.js
@@ -0,0 +1,32 @@
+export const products = [
+  {
+    id: 1,
+    img: "https://cdn.pixabay.com/photo/2021/09/15/21/29/lake-6627781_960_720.jpg",
+    link: "www.google.es",
+  },
+  {
+    id: 2,
+    img: "https://cdn.pixabay.com/photo/2021/09/03/15/37/mountain-6596074_960_720.jpg",
+    link: "www.google.es",
+  },
+  {
+    id: 3,
+    img: "https://cdn.pixabay.com/photo/2021/08/12/10/38/mountains-6540497_960_720.jpg",
+    link: "www.google.es",
+  },
+  {
+    id: 4,
+    img: "https://cdn.pixabay.com/photo/2021/09/17/03/24/forest-6631518_960_720.jpg",
+    link: "www.google.es",
+  },
+  {
+    id: 5,
+    img: "https://cdn.pixabay.com/photo/2021/08/30/21/29/port-6587129_960_720.jpg",
+    link: "www.google.es",
+  },
+  {
+    id: 6,
+    img: "https://cdn.pixabay.com/photo/2021/10/02/09/18/airplane-6674689_960_720.jpg",
+    link: "www.google.es",
+  },
+];
