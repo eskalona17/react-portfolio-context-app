e4ed863207b11fbc757c76804430d7bb59970829
eskalona17 <eskalona_17@hotmail.com>
2021-10-26 13:00:30 +0200

    product list done

--- src/components/product/Product.jsx
+++ src/components/product/Product.jsx
@@ -9,7 +9,7 @@ const Product = ({ img, link }) => {
         <div className="p-circle"></div>
       </div>
       <a href={link} target="_blank" rel="noreferrer">
-        <img src={img} className="p img" alt="" />
+        <img src={img} className="p-img" alt="" />
       </a>
     </div>
   );
