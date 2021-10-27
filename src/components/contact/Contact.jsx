6a5bac78feb306133e6c73f00fcfd94f983664bb
eskalona17 <eskalona_17@hotmail.com>
2021-10-27 13:29:13 +0200

    changed color of the website with context api

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -2,72 +2,76 @@ import "./contact.css";
 import Phone from "../../img/phone.png";
 import Email from "../../img/email.png";
 import Address from "../../img/address.png";
-import { useRef, useState } from "react";
+import { useRef, useState, useContext } from "react";
 import emailjs from "emailjs-com";
+import { ThemeContext } from "../../context";
 
 const Contact = () => {
-  const formRef = useRef();
-  const [done, setDone] = useState(false)
-  const handleSubmit = (e) => {
-    e.preventDefault();
+    const formRef = useRef();
+    const [done, setDone] = useState(false);
+    const theme = useContext(ThemeContext)
+    const darkMode = theme.state.darkMode;
 
-    emailjs
-      .sendForm(
-        "service_gcewmdc",
-        "template_c5wotno",
-        formRef.current,
-        "user_tjGmBPYIrGHFnSSwoc2KG"
-      )
-      .then(
-        (result) => {
-          console.log(result.text);
-          setDone(true)
-        },
-        (error) => {
-          console.log(error.text);
-        }
-      );
-  };
-  return (
-    <div className="c">
-      <div className="c-bg"></div>
-      <div className="c-wrapper">
-        <div className="c-left">
-          <h1 className="c-title">Let's discuss your project</h1>
-          <div className="c-info">
-            <div className="c-info-item">
-              <img src={Phone} alt="" className="c-icon" />
-              +1 23 45 6789
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        emailjs
+            .sendForm(
+                "service_gcewmdc",
+                "template_c5wotno",
+                formRef.current,
+                "user_tjGmBPYIrGHFnSSwoc2KG"
+            )
+            .then(
+                (result) => {
+                    console.log(result.text);
+                    setDone(true)
+                },
+                (error) => {
+                    console.log(error.text);
+                }
+            );
+    };
+    return (
+        <div className="c">
+            <div className="c-bg"></div>
+            <div className="c-wrapper">
+                <div className="c-left">
+                    <h1 className="c-title">Let's discuss your project</h1>
+                    <div className="c-info">
+                        <div className="c-info-item">
+                            <img src={Phone} alt="" className="c-icon" />
+                            +1 23 45 6789
+                        </div>
+                        <div className="c-info-item">
+                            <img src={Email} alt="" className="c-icon" />
+                            test@test.com
+                        </div>
+                        <div className="c-info-item">
+                            <img src={Address} alt="" className="c-icon" />
+                            test 1, Ny
+                        </div>
+                    </div>
+                </div>
+                <div className="c-right">
+                    <p className="c-desc">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda
+                        fugit quae perferendis, possimus sequi error! Praesentium neque
+                        temporibus, ad eum, debitis itaque tenetur iure molestias distinctio
+                        illo laudantium sequi vero?
+                    </p>
+                    <form ref={formRef} onSubmit={handleSubmit}>
+                        <input style={{ backgroundColor: darkMode && "#333" }} type="text" placeholder="Name" name="user_name" />
+                        <input style={{ backgroundColor: darkMode && "#333" }} type="text" placeholder="Subject" name="user_subject" />
+                        <input style={{ backgroundColor: darkMode && "#333" }} type="text" placeholder="Email" name="user_email" />
+                        <textarea style={{ backgroundColor: darkMode && "#333" }} rows="5" placeholder="Message" name="message"></textarea>
+                        <button>Submit</button>
+                        {done && "Thank you..."}
+                    </form>
+                </div>
             </div>
-            <div className="c-info-item">
-              <img src={Email} alt="" className="c-icon" />
-              test@test.com
-            </div>
-            <div className="c-info-item">
-              <img src={Address} alt="" className="c-icon" />
-              test 1, Ny
-            </div>
-          </div>
         </div>
-        <div className="c-right">
-          <p className="c-desc">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda
-            fugit quae perferendis, possimus sequi error! Praesentium neque
-            temporibus, ad eum, debitis itaque tenetur iure molestias distinctio
-            illo laudantium sequi vero?
-          </p>
-          <form ref={formRef} onSubmit={handleSubmit}>
-            <input type="text" placeholder="Name" name="user_name" />
-            <input type="text" placeholder="Subject" name="user_subject" />
-            <input type="text" placeholder="Email" name="user_email" />
-            <textarea rows="5" placeholder="Message" name="message"></textarea>
-            <button>Submit</button>
-            {done && "Thank you..."}
-          </form>
-        </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default Contact;
