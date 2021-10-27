40a2abce4f5efcea1f0720fd8ad1785f882bb4b4
eskalona17 <eskalona_17@hotmail.com>
2021-10-27 23:24:51 +0200

    little fix for the svg image

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -7,71 +7,91 @@ import emailjs from "emailjs-com";
 import { ThemeContext } from "../../context";
 
 const Contact = () => {
-    const formRef = useRef();
-    const [done, setDone] = useState(false);
-    const theme = useContext(ThemeContext)
-    const darkMode = theme.state.darkMode;
+  const formRef = useRef();
+  const [done, setDone] = useState(false);
+  const theme = useContext(ThemeContext);
+  const darkMode = theme.state.darkMode;
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
+  const handleSubmit = (e) => {
+    e.preventDefault();
 
-        emailjs
-            .sendForm(
-                "service_gcewmdc",
-                "template_c5wotno",
-                formRef.current,
-                "user_tjGmBPYIrGHFnSSwoc2KG"
-            )
-            .then(
-                (result) => {
-                    console.log(result.text);
-                    setDone(true)
-                },
-                (error) => {
-                    console.log(error.text);
-                }
-            );
-    };
-    return (
-        <div className="c">
-            <div className="c-bg"></div>
-            <div className="c-wrapper">
-                <div className="c-left">
-                    <h1 className="c-title">Let's discuss your project</h1>
-                    <div className="c-info">
-                        <div className="c-info-item">
-                            <img src={Phone} alt="" className="c-icon" />
-                            +1 23 45 6789
-                        </div>
-                        <div className="c-info-item">
-                            <img src={Email} alt="" className="c-icon" />
-                            test@test.com
-                        </div>
-                        <div className="c-info-item">
-                            <img src={Address} alt="" className="c-icon" />
-                            test 1, Ny
-                        </div>
-                    </div>
-                </div>
-                <div className="c-right">
-                    <p className="c-desc">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda
-                        fugit quae perferendis, possimus sequi error! Praesentium neque
-                        temporibus, ad eum, debitis itaque tenetur iure molestias distinctio
-                        illo laudantium sequi vero?
-                    </p>
-                    <form ref={formRef} onSubmit={handleSubmit}>
-                        <input style={{ backgroundColor: darkMode && "#333" }} type="text" placeholder="Name" name="user_name" />
-                        <input style={{ backgroundColor: darkMode && "#333" }} type="text" placeholder="Subject" name="user_subject" />
-                        <input style={{ backgroundColor: darkMode && "#333" }} type="text" placeholder="Email" name="user_email" />
-                        <textarea style={{ backgroundColor: darkMode && "#333" }} rows="5" placeholder="Message" name="message"></textarea>
-                        <button>Submit</button>
-                        {done && "Thank you..."}
-                    </form>
-                </div>
+    emailjs
+      .sendForm(
+        "service_gcewmdc",
+        "template_c5wotno",
+        formRef.current,
+        "user_tjGmBPYIrGHFnSSwoc2KG"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+          setDone(true);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
+  return (
+    <div className="c">
+      <div className="c-bg"></div>
+      <div className="c-wrapper">
+        <div className="c-left">
+          <h1 className="c-title">Let's discuss your project</h1>
+          <div className="c-info">
+            <div className="c-info-item">
+              <img src={Phone} alt="" className="c-icon" />
+              +1 23 45 6789
             </div>
+            <div className="c-info-item">
+              <img src={Email} alt="" className="c-icon" />
+              test@test.com
+            </div>
+            <div className="c-info-item">
+              <img src={Address} alt="" className="c-icon" />
+              test 1, Ny
+            </div>
+          </div>
         </div>
-    );
+        <div className="c-right">
+          <p className="c-desc">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda
+            fugit quae perferendis, possimus sequi error! Praesentium neque
+            temporibus, ad eum, debitis itaque tenetur iure molestias distinctio
+            illo laudantium sequi vero?
+          </p>
+          <form ref={formRef} onSubmit={handleSubmit}>
+            <input
+              style={{ backgroundColor: darkMode && "#333" }}
+              type="text"
+              placeholder="Name"
+              name="user_name"
+            />
+            <input
+              style={{ backgroundColor: darkMode && "#333" }}
+              type="text"
+              placeholder="Subject"
+              name="user_subject"
+            />
+            <input
+              style={{ backgroundColor: darkMode && "#333" }}
+              type="text"
+              placeholder="Email"
+              name="user_email"
+            />
+            <textarea
+              style={{ backgroundColor: darkMode && "#333" }}
+              rows="5"
+              placeholder="Message"
+              name="message"
+            ></textarea>
+            <button>Submit</button>
+            {done && "Thank you..."}
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Contact;
